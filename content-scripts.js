c1818beaf448bc75c41e8d2e2ba308c14826ec55
Victor Savinov <katashiroku@gmail.com>
2021-01-14 17:27:17 +0300

    v0.5-alpha

--- content-scripts.js
+++ content-scripts.js
@@ -1,32 +1,12 @@
 /*------------------------------------------------------------------------------
->>> UNBLOB
+>>> TABLE OF CONTENTS:
 --------------------------------------------------------------------------------
-# Global variables
-# Inject script
-# User interface
-# Search videos
-# Update rects
-# On mouse over
-# Initialization
+1.0 Inject script
+2.0 Initialization
 ------------------------------------------------------------------------------*/
 
 /*------------------------------------------------------------------------------
-# GLOBAL VARIABLES
-------------------------------------------------------------------------------*/
-
-var unBlob_blobs = {},
-    ui = false,
-    ui_link = false,
-    videos = [],
-    rects = [],
-    mouse = {
-        x: 0,
-        y: 0
-    };
-
-
-/*------------------------------------------------------------------------------
-# INJECT SCRIPT
+1.0 INJECT SCRIPT
 ------------------------------------------------------------------------------*/
 
 function injectScript(string) {
@@ -36,237 +16,22 @@ function injectScript(string) {
 
     document.documentElement.appendChild(script);
 
-    //script.remove();
+    script.remove();
 }
 
 
 /*------------------------------------------------------------------------------
-# USER INTERFACE
+2.0 INITIALIZATION
 ------------------------------------------------------------------------------*/
 
-function createUI() {
-    ui = document.createElement('div');
-    ui_link = document.createElement('a');
+(function() {
+    var textContent = 'var unBlob={';
 
-    ui.className = 'unblob-outline unblob-outline--hidden';
-    ui_link.className = 'unblob-button'
-
-    ui_link.innerHTML = '<svg viewBox="0 0 24 24"><path d="M18 19H6a1 1 0 01-1-1V6c0-.6.5-1 1-1h5c.6 0 1-.5 1-1s-.5-1-1-1H5a2 2 0 00-2 2v14c0 1.1.9 2 2 2h14a2 2 0 002-2v-6c0-.6-.5-1-1-1s-1 .5-1 1v5c0 .6-.5 1-1 1zM14 4c0 .6.5 1 1 1h2.6l-9.1 9.1a1 1 0 101.4 1.4l9.1-9V9c0 .6.5 1 1 1s1-.5 1-1V4c0-.6-.5-1-1-1h-5a1 1 0 00-1 1z"/></svg>';
-
-    ui.appendChild(ui_link);
-
-    document.body.appendChild(ui);
-}
-
-function resizeUI(rect) {
-    if (ui.offsetLeft !== rect.left) {
-        ui.style.left = rect.left + 'px';
+    for (var key in unBlob) {
+        textContent += key + ': ' + unBlob[key] + ',';
     }
 
-    if (ui.offsetTop !== rect.top) {
-        ui.style.top = rect.top + 'px';
-    }
+    textContent += '};unBlob.init();';
 
-    if (ui.offsetWidth !== rect.width) {
-        ui.style.width = rect.width + 'px';
-    }
-
-    if (ui.offsetHeight !== rect.height) {
-        ui.style.height = rect.height + 'px';
-    }
-}
-
-
-/*------------------------------------------------------------------------------
-# SEARCH VIDEOS
-------------------------------------------------------------------------------*/
-
-function searchVideos() {
-    var unlisted_video = document.querySelector('video:not(.unblob-video)');
-
-    if (unlisted_video) {
-        unlisted_video.classList.add('unblob-video');
-
-        videos.push(unlisted_video);
-        rects.push({
-            index: rects.length
-        });
-
-        updateRects();
-    }
-}
-
-
-/*------------------------------------------------------------------------------
-# UPDATE RECTS
-------------------------------------------------------------------------------*/
-
-function updateRects() {
-    for (var i = 0, l = videos.length; i < l; i++) {
-        var video = videos[i];
-
-        if (video) {
-            var rect = rects[i],
-                data = video.getBoundingClientRect();
-
-            rect.left = data.left;
-            rect.top = data.top;
-            rect.width = data.width;
-            rect.height = data.height;
-        }
-    }
-
-    isHover();
-}
-
-window.addEventListener('scroll', updateRects);
-window.addEventListener('mousewheel', updateRects);
-
-window.addEventListener('resize', function() {
-    setTimeout(function() {
-        updateRects();
-    }, 100);
-});
-
-
-/*------------------------------------------------------------------------------
-# ON MOUSE OVER
-------------------------------------------------------------------------------*/
-
-function isHover() {
-    var active = false;
-
-    for (var i = 0, l = rects.length; i < l; i++) {
-        var rect = rects[i];
-
-        if (
-            mouse.x > rect.left &&
-            mouse.y > rect.top &&
-            mouse.x < rect.left + rect.width &&
-            mouse.y < rect.top + rect.height
-        ) {
-            active = rect;
-        }
-    }
-
-    if (active) {
-        if (ui) {
-            ui.classList.remove('unblob-outline--hidden');
-        }
-
-        var found = unBlob_blobs[videos[active.index].src];
-
-        if (found) {
-            ui_link.href = found;
-        } else {
-            ui_link.href = videos[active.index].src;
-        }
-
-        resizeUI(active);
-    } else if (ui) {
-        ui.classList.add('unblob-outline--hidden');
-    }
-}
-
-window.addEventListener('mousemove', function(event) {
-    mouse.x = event.clientX;
-    mouse.y = event.clientY;
-
-    isHover();
-});
-
-
-/*------------------------------------------------------------------------------
-# INITIALIZATION
-------------------------------------------------------------------------------*/
-
-window.addEventListener('DOMContentLoaded', function() {
-    createUI();
-
-    setInterval(searchVideos, 1000);
-});
-
-injectScript(`
-    var unBlob_blobs = {};
-
-    XMLHttpRequestPrototypeOpen = XMLHttpRequest.prototype.open;
-    SourceBufferPrototypeAppendBuffer = SourceBuffer.prototype.appendBuffer;
-
-    MediaSource.prototype.blobSrc = false;
-    ArrayBuffer.prototype.URL = false;
-    SourceBuffer.prototype.URL = false;
-    Uint8Array.prototype.URL = false;
-
-
-    URL.createObjectURL = (function(original) {
-        return function() {
-            var result = original.apply(this, arguments);
-            
-            //console.log('createObjectURL', arguments, result);
-
-            this.blobSrc = result;
-
-            unBlob_blobs[result] = arguments[0];
-            //unBlob_blobs[result] = arguments[0].sourceBuffers[0].URL.replace(/(\\&|\\?)range\\=[^&]*/, '');
-            
-            return result;
-        }
-    })(URL.createObjectURL);
-
-
-    XMLHttpRequest.prototype.open = function() {
-        this.addEventListener('load', function() {
-            this.response.URL = this.responseURL;
-
-            //console.log('XMLHttpRequest', this.response);
-        });
-
-        XMLHttpRequestPrototypeOpen.apply(this, arguments, arguments.callee);
-    };
-
-
-    Uint8Array = (function(Uint8Array) {
-        return function(buffer, byteOffset, length) {
-            var a;
-
-            a = new Uint8Array(buffer, byteOffset, length);
-
-            a.URL = buffer.URL;
-
-            //console.log('Uint8Array', a);
-
-            return a;
-        };
-    }(Uint8Array));
-
-
-    SourceBuffer.prototype.appendBuffer = function() {
-        var a = SourceBufferPrototypeAppendBuffer.apply(this, arguments);
-
-        this.URL = arguments[0].URL;
-
-        //console.log('SourceBuffer', arguments, a);
-
-        var data = {};
-
-        for (var key in unBlob_blobs) {
-            var item = unBlob_blobs[key];
-
-            if (
-                item.sourceBuffers &&
-                item.sourceBuffers[0] &&
-                item.sourceBuffers[0].URL
-            ) {
-                data[key] = item.sourceBuffers[0].URL.replace(/(\\&|\\?)range\\=[^&]*/, '');
-            }
-        }
-
-        document.dispatchEvent(new CustomEvent('unblob-updated-source-buffer', {
-            detail: data
-        }));
-    };
-`);
-
-document.addEventListener('unblob-updated-source-buffer', function(event) {
-    unBlob_blobs = event.detail;
-});
+    injectScript(textContent);
+})();
---
+++ website-scripts.js
@@ -0,0 +1,304 @@
+/*------------------------------------------------------------------------------
+>>> TABLE OF CONTENTS:
+--------------------------------------------------------------------------------
+1.0 Global variable
+2.0 Modifiers
+  2.1 Create object URL
+  2.2 XMLHttpRequest
+  2.3 Uint8Array
+  2.4 Append buffer
+3.0 User interface
+  3.1 Create
+  3.2 Resize
+4.0 Media observer
+5.0 Get bounding client rect
+6.0 Mouse over
+7.0 Initialization
+------------------------------------------------------------------------------*/
+
+/*------------------------------------------------------------------------------
+1.0 GLOBAL VARIABLE
+--------------------------------------------------------------------------------
+The variable "unBlob" is used on the website side.
+------------------------------------------------------------------------------*/
+
+var unBlob = {
+    active: false,
+    blobs: '{}',
+    media: '[]',
+    mouse: '{x: 0,y: 0}',
+    rects: '[]'
+};
+
+
+/*------------------------------------------------------------------------------
+2.0 MODIFIERS
+--------------------------------------------------------------------------------
+The modification of JavaScript's standard, built-in objects.
+------------------------------------------------------------------------------*/
+
+/*------------------------------------------------------------------------------
+2.1 CREATE OBJECT URL
+--------------------------------------------------------------------------------
+The modifier of the "URL.createObjectURL()" method.
+------------------------------------------------------------------------------*/
+
+unBlob.createObjectURL = function() {
+    URL.createObjectURL = (function(original) {
+        return function(source) {
+            var value = original.apply(this, arguments);
+
+            unBlob.blobs[value] = arguments[0];
+
+            //console.log('createObjectURL', this, arguments);
+
+            return value;
+        };
+    }(URL.createObjectURL));
+};
+
+
+/*------------------------------------------------------------------------------
+2.2 XMLHttpRequest
+--------------------------------------------------------------------------------
+The modifier of the "XMLHttpRequest.prototype.open()" method.
+------------------------------------------------------------------------------*/
+
+unBlob.XMLHttpRequest = function() {
+    XMLHttpRequest.prototype.open = (function(original) {
+        return function() {
+            this.addEventListener('load', function() {
+                this.response.URL = this.responseURL;
+
+                //console.log('XMLHttpRequest', this.response);
+            });
+
+            original.apply(this, arguments);
+        };
+    }(XMLHttpRequest.prototype.open));
+};
+
+
+/*------------------------------------------------------------------------------
+2.3 UINT8ARRAY
+--------------------------------------------------------------------------------
+The modifier of the "Uint8Array()" constructor.
+------------------------------------------------------------------------------*/
+
+unBlob.Uint8Array = function() {
+    Uint8Array.prototype.URL = '';
+
+    Uint8Array = (function(original) {
+        return function(buffer, byteOffset, length) {
+            var object = new original(buffer, byteOffset, length);
+
+            if (typeof buffer === 'object') {
+                object.URL = buffer.URL;
+            }
+
+            //console.log('Uint8Array', object, arguments);
+
+            return object;
+        };
+    }(Uint8Array));
+};
+
+
+/*------------------------------------------------------------------------------
+2.4 APPENDBUFFER
+--------------------------------------------------------------------------------
+The modifier of the "SourceBuffer.appendBuffer()" method.
+------------------------------------------------------------------------------*/
+
+unBlob.appendBuffer = function() {
+    SourceBuffer.prototype.appendBuffer = (function(original) {
+        return function(source) {
+            this.URL = source.URL;
+
+            //console.log('appendBuffer', this, source.URL);
+
+            original.apply(this, arguments);
+        };
+    }(SourceBuffer.prototype.appendBuffer));
+};
+
+
+/*------------------------------------------------------------------------------
+3.0 USER INTERFACE
+------------------------------------------------------------------------------*/
+
+/*------------------------------------------------------------------------------
+3.1 CREATE
+------------------------------------------------------------------------------*/
+
+unBlob.createUserInterface = function() {
+    var container = document.createElement('div'),
+        link = document.createElement('a');
+
+    container.className = 'unblob-container--hidden';
+
+    link.className = 'unblob-button'
+
+    link.innerHTML = '<svg viewBox="0 0 24 24"><path d="M18 19H6a1 1 0 01-1-1V6c0-.6.5-1 1-1h5c.6 0 1-.5 1-1s-.5-1-1-1H5a2 2 0 00-2 2v14c0 1.1.9 2 2 2h14a2 2 0 002-2v-6c0-.6-.5-1-1-1s-1 .5-1 1v5c0 .6-.5 1-1 1zM14 4c0 .6.5 1 1 1h2.6l-9.1 9.1a1 1 0 101.4 1.4l9.1-9V9c0 .6.5 1 1 1s1-.5 1-1V4c0-.6-.5-1-1-1h-5a1 1 0 00-1 1z"/></svg>';
+
+    container.appendChild(link);
+
+    document.body.appendChild(container);
+
+    unBlob.ui = container;
+    unBlob.ui_link = link;
+};
+
+
+/*------------------------------------------------------------------------------
+3.2 RESIZE
+------------------------------------------------------------------------------*/
+
+
+unBlob.resizeUserInterface = function(rect) {
+    if (unBlob.ui.offsetLeft !== rect.left) {
+        unBlob.ui.style.left = rect.left + 'px';
+    }
+
+    if (unBlob.ui.offsetTop !== rect.top) {
+        unBlob.ui.style.top = rect.top + 'px';
+    }
+
+    if (unBlob.ui.offsetWidth !== rect.width) {
+        unBlob.ui.style.width = rect.width + 'px';
+    }
+
+    if (unBlob.ui.offsetHeight !== rect.height) {
+        unBlob.ui.style.height = rect.height + 'px';
+    }
+};
+
+
+/*------------------------------------------------------------------------------
+4.0 MEDIA OBSERVER
+------------------------------------------------------------------------------*/
+
+unBlob.observe = function() {
+    setInterval(function() {
+        var elements = document.querySelectorAll('audio:not(.unblob-media),video:not(.unblob-media)');
+
+        for (var i = 0, l = elements.length; i < l; i++) {
+            var element = elements[i],
+                data = element.getBoundingClientRect();
+
+            element.classList.add('unblob-media');
+
+            unBlob.media.push(element);
+
+            unBlob.rects.push({
+                index: unBlob.rects.length
+            });
+
+            var rect = unBlob.rects[unBlob.rects.length];
+
+            rect.left = data.left;
+            rect.top = data.top;
+            rect.width = data.width;
+            rect.height = data.height;
+        }
+    }, 1000);
+};
+
+
+/*------------------------------------------------------------------------------
+5.0 GET BOUNDING CLIENT RECT
+------------------------------------------------------------------------------*/
+
+unBlob.updateRects = function() {
+    for (var i = 0, l = unBlob.media.length; i < l; i++) {
+        var media = unBlob.media[i];
+
+        if (media) {
+            var rect = unBlob.rects[i],
+                data = media.getBoundingClientRect();
+
+            rect.left = data.left;
+            rect.top = data.top;
+            rect.width = data.width;
+            rect.height = data.height;
+        }
+    }
+};
+
+
+/*------------------------------------------------------------------------------
+6.0 MOUSE OVER
+------------------------------------------------------------------------------*/
+
+unBlob.mouseOver = function() {
+    var active = false;
+
+    for (var i = 0, l = unBlob.rects.length; i < l; i++) {
+        var rect = unBlob.rects[i];
+
+        if (
+            unBlob.mouse.x > rect.left &&
+            unBlob.mouse.y > rect.top &&
+            unBlob.mouse.x < rect.left + rect.width &&
+            unBlob.mouse.y < rect.top + rect.height
+        ) {
+            active = rect;
+        }
+    }
+
+    if (active) {
+        if (unBlob.ui) {
+            unBlob.ui.className = 'unblob-container';
+        }
+
+        var found = unBlob.blobs[unBlob.media[active.index].src];
+
+        if (
+            found &&
+            found.activeSourceBuffers &&
+            found.activeSourceBuffers[0] &&
+            found.activeSourceBuffers[0].URL
+        ) {
+            unBlob.ui_link.href = found.activeSourceBuffers[0].URL.replace(/(\&|\?)range\=[^&]*/, '');
+        } else {
+            unBlob.ui_link.href = unBlob.media[active.index].src;
+        }
+
+        unBlob.resizeUserInterface(active);
+    } else if (unBlob.ui) {
+        unBlob.ui.className = 'unblob-container--hidden';
+    }
+};
+
+
+/*------------------------------------------------------------------------------
+7.0 INITIALIZATION
+------------------------------------------------------------------------------*/
+
+unBlob.init = function() {
+    this.createObjectURL();
+    this.XMLHttpRequest();
+    this.Uint8Array();
+    this.appendBuffer();
+
+    window.addEventListener('DOMContentLoaded', function() {
+        unBlob.createUserInterface();
+        unBlob.observe();
+
+        window.addEventListener('scroll', unBlob.updateRects);
+        window.addEventListener('mousewheel', unBlob.updateRects);
+
+        window.addEventListener('resize', function() {
+            setTimeout(function() {
+                unBlob.updateRects();
+            }, 100);
+        });
+
+        window.addEventListener('mousemove', function(event) {
+            unBlob.mouse.x = event.clientX;
+            unBlob.mouse.y = event.clientY;
+
+            unBlob.mouseOver();
+        });
+    });
+};
